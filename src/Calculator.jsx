3928904e721fb63bb1f640e11ef64c66f26b4764
Morten Lohne <lohnemorten@gmail.com>
2024-12-20 23:40:41 +0100

    Formatting

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -4,41 +4,42 @@ import './Calculator.css'; // Assuming you have a CSS file for styles
 import Worker from './calc.worker.js';
 
 const types = [
-    "Normal",
-    "Fire",
-    "Water",
-    "Electric",
-    "Grass",
-    "Ice",
-    "Fighting",
-    "Poison",
-    "Ground",
-    "Flying",
-    "Psychic",
-    "Bug",
-    "Rock",
-    "Ghost",
-    "Dragon",
-    "Dark",
-    "Steel"
+  "Normal",
+  "Fire",
+  "Water",
+  "Electric",
+  "Grass",
+  "Ice",
+  "Fighting",
+  "Poison",
+  "Ground",
+  "Flying",
+  "Psychic",
+  "Bug",
+  "Rock",
+  "Ghost",
+  "Dragon",
+  "Dark",
+  "Steel"
 ];
 
 const phrases = [
-    "FreeSpirited",
-    "Preparation",
-    "SlowAndSteady",
-    "Endurance",
-    "HighRiskHighReturn",
-    "Weakening",
-    "Unpredictable",
-    "BattleFlow",
-    "Adaptable",
+  "FreeSpirited",
+  "Preparation",
+  "SlowAndSteady",
+  "Endurance",
+  "HighRiskHighReturn",
+  "Weakening",
+  "Unpredictable",
+  "BattleFlow",
+  "Adaptable",
 ]
 
 
 const Calculator = () => {
   const [type, setType] = useState("");
   const [phrase, setPhrase] = useState("");
+  const [excludedSpecies, setExcludedSpecies] = useState([]); // Species on our team or in last round's pool
   const [result, setResult] = useState(null);
   const [worker] = useState(() => new Worker());
 
@@ -51,17 +52,17 @@ const Calculator = () => {
   };
 
   useEffect(() => {
-    worker.postMessage(JSON.stringify({type: type, phrase: phrase}));
-    }, [type, phrase, worker]);
+    worker.postMessage(JSON.stringify({ type: type, phrase: phrase }));
+  }, [type, phrase, worker]);
 
   worker.onmessage = (e) => {
     setResult(JSON.parse(e.data));
   };
 
-  const probabilityPerSpecies = (result || []).reduce((acc, {pokemon, probability}) => {
+  const probabilityPerSpecies = (result || []).reduce((acc, { pokemon, probability }) => {
     acc[pokemon.species] = (acc[pokemon.species] || 0) + probability
     return acc
-    
+
   }, {});
 
   let resultPerSpecies = Object.entries(probabilityPerSpecies);
@@ -75,65 +76,65 @@ const Calculator = () => {
           <option value="">Unknown type</option>
           <option value="Typeless">No type</option>
           {types.map(type =>
-              <option key={type} value={type}>{type}</option>
+            <option key={type} value={type}>{type}</option>
           )}
 
-          </select>
+        </select>
         <select value={phrase} onChange={handlePhraseChange}>
           <option value="">Unknown phrase</option>
           {phrases.map(phrase =>
-              <option key={phrase} value={phrase}>{phrase}</option>
+            <option key={phrase} value={phrase}>{phrase}</option>
           )}
 
-          </select>
+        </select>
       </div>
-      {result === null ? <p>Loading Pokemon data...</p> : result.length === 0 ? <p>No matching Pokemon found!</p> : 
-      <div className="table-container">
-      <table className="table">
-        <caption>
-            Result 
-        </caption>
-        <thead>
-            <tr>
-            <th scope="col">Pokemon</th>
-            <th scope="col">Probability</th>
-            </tr>
-        </thead>
-        <tbody>
-            { result
-              .filter(({probability}, i) => probability > 0.01 || i < 20)
-              .map(res =>
-            <tr key={res.pokemon.species + "-" + res.pokemon.id}>
-                <th scope="row">{res.pokemon.species + "-" + res.pokemon.id}</th>
-                <td>{(res.probability * 100).toFixed(2) + "%"}</td>
-            </tr>
-            
-            ) }
-        </tbody>
-        </table>      
-        <table className="table">
-        <caption>
-            Result per species
-        </caption>
-        <thead>
-            <tr>
-            <th scope="col">Species</th>
-            <th scope="col">Probability</th>
-            </tr>
-        </thead>
-        <tbody>
-            { resultPerSpecies
-                .map(([species, probability]) => 
-            <tr key={species}>
-                <th scope="row">{species}</th>
-                <td>{(probability * 100).toFixed(2) + "%"}</td>
-            </tr>
-            
-            ) }
-        </tbody>
-        </table>
+      {result === null ? <p>Loading Pokemon data...</p> : result.length === 0 ? <p>No matching Pokemon found!</p> :
+        <div className="table-container">
+          <table className="table">
+            <caption>
+              Result
+            </caption>
+            <thead>
+              <tr>
+                <th scope="col">Pokemon</th>
+                <th scope="col">Probability</th>
+              </tr>
+            </thead>
+            <tbody>
+              {result
+                .filter(({ probability }, i) => probability > 0.01 || i < 20)
+                .map(res =>
+                  <tr key={res.pokemon.species + "-" + res.pokemon.id}>
+                    <th scope="row">{res.pokemon.species + "-" + res.pokemon.id}</th>
+                    <td>{(res.probability * 100).toFixed(2) + "%"}</td>
+                  </tr>
+
+                )}
+            </tbody>
+          </table>
+          <table className="table">
+            <caption>
+              Result per species
+            </caption>
+            <thead>
+              <tr>
+                <th scope="col">Species</th>
+                <th scope="col">Probability</th>
+              </tr>
+            </thead>
+            <tbody>
+              {resultPerSpecies
+                .map(([species, probability]) =>
+                  <tr key={species}>
+                    <th scope="row">{species}</th>
+                    <td>{(probability * 100).toFixed(2) + "%"}</td>
+                  </tr>
+
+                )}
+            </tbody>
+          </table>
         </div>
-        }
+      }
     </div>
   );
 };
